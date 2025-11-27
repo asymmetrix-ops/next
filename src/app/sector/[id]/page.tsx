95cfc05c9353a733a9bd9082b7ad7b7a28ba56e9
asymmetrix-ops <asymmetrix@asymmetrixintelligence.com>
2025-11-27 10:33:00 +0000

    removed server side await

--- src/app/sector/[id]/page.tsx
+++ src/app/sector/[id]/page.tsx
@@ -1,62 +1,9 @@
-import { serverDashboardApiService } from "@/lib/server-auth";
 import SectorDetailPage from "./_components/SectorDetailClient";
 
 export const dynamic = "force-dynamic";
 
-interface PageProps {
-  params: {
-    id: string;
-  };
-}
-
-async function fetchSectorData(sectorId: string) {
-  try {
-    const [
-      sectorData,
-      marketMap,
-      strategicAcquirers,
-      peInvestors,
-      recentTransactions,
-    ] = await Promise.all([
-      serverDashboardApiService.getSector(sectorId).catch(() => null),
-      serverDashboardApiService.getSectorMarketMap(sectorId).catch(() => null),
-      serverDashboardApiService.getSectorStrategicAcquirers(sectorId).catch(() => null),
-      serverDashboardApiService.getSectorPEInvestors(sectorId).catch(() => null),
-      serverDashboardApiService.getSectorRecentTransactions(sectorId).catch(() => null),
-    ]);
-
-    return {
-      sectorData,
-      marketMap,
-      strategicAcquirers,
-      peInvestors,
-      recentTransactions,
-    };
-  } catch (error) {
-    console.error("[Server] Error fetching sector data:", error);
-    return {
-      sectorData: null,
-      marketMap: null,
-      strategicAcquirers: null,
-      peInvestors: null,
-      recentTransactions: null,
-    };
-  }
-}
-
-export default async function SectorPage({ params }: PageProps) {
-  const { id } = params;
-  
-  // Fetch all initial data server-side
-  const initialData = await fetchSectorData(id);
-
-  return (
-    <SectorDetailPage
-      initialSectorData={initialData.sectorData}
-      initialMarketMap={initialData.marketMap}
-      initialStrategicAcquirers={initialData.strategicAcquirers}
-      initialPEInvestors={initialData.peInvestors}
-      initialRecentTransactions={initialData.recentTransactions}
-    />
-  );
+export default function SectorPage() {
+  // Let the client-side component progressively fetch all sector data
+  // so navigation to this page is not blocked by server-side API calls.
+  return <SectorDetailPage />;
 }
